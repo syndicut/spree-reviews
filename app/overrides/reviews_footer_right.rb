12d8bcc0b239365e75c55ca6a395bba9ececf84e
Сергей <serge@serge-desktop.(none)>
2012-02-07 23:56:50 +0400

    overrides updated

--- app/overrides/reviews_footer_right.rb
+++ app/overrides/reviews_footer_right.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-    :virtual_path => "shared/_footer",
-    :name => "reviews_footer_right",
-    :insert_after => "[data-hook='footer_right']",
-    :text => "<%= javascript_include_tag('jquery.rating.js') %>
-  <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>")
+  :virtual_path => "layouts/spree_application",
+  :name => "reviews_footer_right",
+  :insert_after => "#footer-right",
+  :text => "<%= javascript_include_tag('jquery.rating.js') %>
+  <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>",
+  :disabled => false)
