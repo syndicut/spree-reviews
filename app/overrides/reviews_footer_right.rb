bce9ff35e8ad60998ec0bc271349429549e19fdd
Rashit Azizbaev <syndicut@gmail.com>
2012-05-15 20:14:44 +0400

    Changed hook target

--- app/overrides/reviews_footer_right.rb
+++ app/overrides/reviews_footer_right.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "layouts/spree_application",
   :name => "reviews_footer_right",
-  :insert_after => "#footer-right",
+  :insert_after => "[data-hook='footer_right']",
   :text => "<%= javascript_include_tag('jquery.rating.js') %>
   <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>",
   :disabled => false)
