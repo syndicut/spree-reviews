b0b33b7e14b78811296e65422e0fb13541e809d5
Rashit Azizbaev <syndicut@gmail.com>
2012-05-15 16:44:36 +0400

    Fixed path to reviews.css

--- app/overrides/reviews_inside_head.rb
+++ app/overrides/reviews_inside_head.rb
@@ -2,4 +2,4 @@ Deface::Override.new(
     :virtual_path => "layouts/spree_application",
     :name => "reviews_inside_head",
     :insert_bottom => "[data-hook='inside_head']",
-    :text => "<%= stylesheet_link_tag('reviews.css') %>")
+    :text => "<%= stylesheet_link_tag('store/reviews') %>")
